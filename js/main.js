6671e8535d63710f3fbefe8b36ef409e19d43cfd
dave-woods <dwoods@tcd.ie>
2016-06-02 10:01:01 +0100

    Testing js loading of files into ul elem

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
-	$(".button").on("click", function(){
+	$(".index-wrap .button").on("click", function(){
 		$this = $(this);
 		$this.css("background-color", "#2493DA");
 		setTimeout(function(){
 			$this.css("background-color", "#fff");
 		}, 2000);
+	});
+
+	$(".intern-report-wrap").ready(function(){
+		var dir = ".";
+		var fileextension = ".html";
+		$.ajax({
+			url: dir,
+			success: function (data) {
+				$(data).find("a:contains(" + fileextension + ")").each(function () {
+					var filename = this.href.replace(window.location.host, "").replace("http:///", "");
+					$(".report-list").append($("<li><a href=\"" + filename + "\">" + filename + "</a></li>"));
+				});
+			}
+		});
 	});
 });
